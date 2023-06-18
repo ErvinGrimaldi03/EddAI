27282e866835f3c818ff7508e9771fe7c13c74e0
GrimaldiErvin03 <grimaldiervin@gmail.com>
2023-06-18 09:06:50 -0700

    temp

--- frontend/camera_prompt.py
+++ frontend/camera_prompt.py
@@ -3,36 +3,27 @@ import cv2
 
 def camera():
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    output_file = '.././backend/data/camera_video.mp4'
+    output_file = './backend/data/camera_video.mp4'
     frame_rate = 24
     frame_size = (640, 480)
     video_writer = cv2.VideoWriter(output_file, fourcc, frame_rate, frame_size)
 
-
     cap = cv2.VideoCapture(0)
-
 
     if not cap.isOpened():
         print("Failed to open the camera")
         exit()
 
-
     while True:
-
         ret, frame = cap.read()
 
-
         if ret:
-
             cv2.imshow('Camera', frame)
-
-
             video_writer.write(frame)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(1) == ord('q'):
             break
 
     video_writer.release()
     cap.release()
     cv2.destroyAllWindows()
-camera()
