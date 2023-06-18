de10cc030a077e94244ddfd5f0dfd70a31ad5bfe
GrimaldiErvin03 <grimaldiervin@gmail.com>
2023-06-17 21:23:12 -0700

    convert_to_audio.py has added functionalities:
    Check if file exists
    Check if destination and source file are the same

--- backend/convert_to_audio.py
+++ backend/convert_to_audio.py
@@ -1,5 +1,16 @@
 import moviepy.editor as moviepy
+import os.path as op
 
-def extract_mp3():
-    clip = moviepy.VideoFileClip(r"./data/test.mp4")
-    clip.audio.write_audiofile(r"./data/test.mp3")
+def extract_mp3(video, audio):
+    if (video == audio):
+        print("ERROR: source and destination file are the same")
+        exit() # TODO CHANGE THIS TO SOMETHING THAT DOESNT STOP RUNNING
+    video = f"./data/{video}"
+    audio = f"./data/{audio}"
+
+    if (op.isfile(audio)):
+        print("ERROR: destination file exists already!")
+
+    else:
+        clip = moviepy.VideoFileClip(video)
+        clip.audio.write_audiofile(audio)
